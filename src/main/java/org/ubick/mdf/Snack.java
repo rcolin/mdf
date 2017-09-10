5da3fea5e1e0acc9ef50d56951a8a2c5fe0b8139
rcolin <remi.collin@gmail.com>
2017-09-10 16:43:16 +0200

    add snack

--- src/main/java/org/ubick/mdf/Snack.java
+++ src/main/java/org/ubick/mdf/Snack.java
@@ -1,25 +1,78 @@
 package org.ubick.mdf;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Snack {
 
     public static void main( String[] argv ) throws Exception {
-        String  line;
-        Scanner sc = new Scanner(System.in);
+            		
+    		Scanner sc = null;
+    	    if (argv!=null && argv.length>0 && argv[0].equals("-d")){
+    	    		try {
+    					sc = new Scanner(new File(argv[1]));
+    				} catch (FileNotFoundException e) {
+    					e.printStackTrace();
+    				}
+    	    } else {
+    	    		sc = new Scanner(System.in);
+    	    }
 
-	    int n = sc.nextInt();
-        int p = sc.nextInt();
+	    int n = Integer.parseInt(sc.nextLine());
+        int p = Integer.parseInt(sc.nextLine());
+        
+        List<Integer> snackx = new ArrayList<>();
+        List<Integer> snacky = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+        		snackx.add(i);
+        		snacky.add(0);	
+		}
+        
+        //pourquoi ?
+        //sc.nextLine();
         
         String[] mvt = new String[p];
         for(int i = 0; i < p; i++){
-            String m = sc.nextLine();
-            mvt[i] = m;
+        		String mv = sc.nextLine();
+        		mvt[i] = mv;
         }
-
+        
         for(String m : mvt){
-             //switch
-
+        		
+        		if(m.equals("D")) {
+        			
+        			snackx.add(snackx.get(snackx.size()-1)+1);
+        			snacky.add(snacky.get(snacky.size()-1));
+        			snackx.remove(0);
+        			snacky.remove(0);
+        			
+        		}else if (m.equals("B")) {
+        			
+        			snackx.add(snackx.get(snackx.size()-1));
+        			snacky.add(snacky.get(snacky.size()-1)+1);
+        			snackx.remove(0);
+        			snacky.remove(0);
+        			
+        		}else if (m.equals("G")) {
+        			
+        			snackx.add(snackx.get(snackx.size()-1)-1);
+        			snacky.add(snacky.get(snacky.size()-1));
+        			snackx.remove(0);
+        			snacky.remove(0);
+        			
+        		}else if (m.equals("H")) {
+        			
+        			snackx.add(snackx.get(snackx.size()-1));
+        			snacky.add(snacky.get(snacky.size()-1)-1);
+        			snackx.remove(0);
+        			snacky.remove(0);
+        			
+        		}
         }
+        
+        System.out.println(snackx.get(0) + " " + snacky.get(0));
+     
     }
 }
