67d8b30fae4536e837e5e40e3902954945255d48
rcolin <remi.collin@gmail.com>
2017-09-13 07:15:21 +0200

    add algo générique combinatoire sur string

---
+++ src/main/java/org/ubick/mdf/TestComb.java
@@ -0,0 +1,29 @@
+package org.ubick.mdf;
+
+public class TestComb {
+
+	   public static void main(String[] args)
+	   {
+	      String[] ensemble = { "A", "B", "C", "D" };
+	      int profondeur = 10;
+	      displayEnsemble(ensemble, profondeur, 0, "", 0);
+	   }
+	   
+	   public static void displayEnsemble(String[] ensemble, int profMax,
+	         int profCourante, String prefix, int rang)
+	   {
+	      if (profCourante < profMax)
+	      {
+	         for (int i = rang; i < ensemble.length; i++)
+	         {
+	            System.out.println(prefix + ensemble[i]);
+	         }
+	 
+	         for (int i = rang; i < ensemble.length; i++)
+	         {
+	            displayEnsemble(ensemble, profMax, profCourante + 1, prefix
+	                  + ensemble[i], i + 1);
+	         }
+	      }
+	   }
+}
