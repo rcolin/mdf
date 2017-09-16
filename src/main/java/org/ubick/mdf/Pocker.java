244dab62d52fc7bf689e90e0190f64e73fc51ac8
rcolin <remi.collin@gmail.com>
2017-09-16 15:26:24 +0200

    update Pocker

---
+++ src/main/java/org/ubick/mdf/Pocker.java
@@ -0,0 +1,35 @@
+package org.ubick.mdf;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Pocker {
+
+	public static void main(String[] argv) throws Exception {
+
+		Scanner sc = null;
+		if (argv != null && argv.length > 0 && argv[0].equals("-d")) {
+			try {
+				sc = new Scanner(new File(argv[1]));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			sc = new Scanner(System.in);
+		}
+		//code
+		
+		int sum = sc.nextInt();
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			sum += (-x+y);
+			if(sum < 0)
+				sum = 0;
+		}
+		System.out.println(sum);
+	}
+}
