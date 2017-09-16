1070afa0009339563dfeac67899256c41987648b
rcolin <remi.collin@gmail.com>
2017-09-16 08:25:04 +0200

    optimisation trivial poursuite

--- src/main/java/org/ubick/mdf/TrivialPoursuite.java
+++ src/main/java/org/ubick/mdf/TrivialPoursuite.java
@@ -8,45 +8,33 @@ import java.util.Scanner;
 
 public class TrivialPoursuite {
 
-		public static void main( String[] argv ) throws Exception {
-    		
-    		Scanner sc = null;
-    	    if (argv!=null && argv.length>0 && argv[0].equals("-d")){
-    	    		try {
-    					sc = new Scanner(new File(argv[1]));
-    				} catch (FileNotFoundException e) {
-    					e.printStackTrace();
-    				}
-    	    } else {
-    	    		sc = new Scanner(System.in);
-    	    }
+	public static void main(String[] argv) throws Exception {
 
-    	    Map<Integer, String> colors = new HashMap<>();
-    	    colors.put(1,"orange");
-    	    colors.put(2,"jaune");
-    	    colors.put(3,"vert");
-    	    colors.put(4,"rose");
-    	    colors.put(5,"bleu");
-    	    colors.put(6,"violet");
-    	    
-    	    
-    	    int position = Integer.MAX_VALUE; 
-    	    		
-    	    do {
-    	    		int n = sc.nextInt();
-    	    		if(position == Integer.MAX_VALUE) {
-    	    			position = n;
-    	    		}else {
-    	    			if((position + n) <= 6) {
-    	    				position = position + n;
-    	    			}else {
-    	    				position = n + position - 6;
-    	    			}
-    	    		}
-    	    }while(sc.hasNext());
-    	    
-    	    System.out.println(colors.get(position));
-    	    
+		Scanner sc = null;
+		if (argv != null && argv.length > 0 && argv[0].equals("-d")) {
+			try {
+				sc = new Scanner(new File(argv[1]));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			sc = new Scanner(System.in);
+		}
+
+		String[] colors = { "orange", "jaune", "vert", "rose", "bleu", "violet" };
+
+		int position = 0;
+
+		do {
+			int n = sc.nextInt();
+			position = position + n;
+			if (position > 6) {
+				position = position % 6;
+			}
+		} while (sc.hasNext());
+
+		System.out.println(colors[position - 1]);
+
 	}
 
 }
